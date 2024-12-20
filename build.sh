4618fbda4a8954e2440153d9c455688616d9c80b
Noah ‘s <noahs@Noahs-MacBook-Pro.local>
2024-12-20 22:35:00 +0900

    fix: Update build configuration

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-set -ex  # 에러 발생 시 스크립트 중단 및 실행 명령어 출력
+set -ex
 
-# 현재 디렉토리 확인
-pwd
-ls -la
-
-# Flutter SDK 다운로드 (안정 버전으로 지정)
+# Flutter SDK 다운로드
 git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:$PWD/flutter/bin"
 
-# Flutter 직접 실행
-./flutter/bin/flutter --version
-./flutter/bin/flutter clean
-./flutter/bin/flutter pub get
-./flutter/bin/flutter config --enable-web
-./flutter/bin/flutter build web --release --web-renderer html
+# Flutter 설정 및 빌드
+flutter precache
+flutter config --enable-web
+flutter pub get
+flutter build web --release
 
-# 빌드 결과물 확인
-ls -la build/web
+# Netlify Functions 설정
+cd netlify/functions
+npm install
+cd ../..
